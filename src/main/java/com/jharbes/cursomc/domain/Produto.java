da933fac459f6bf057c8c0b20bbe6b0b7d0e08a6
Jorge Harbes <jharbes@hotmail.com>
2022-11-20 12:17:02 -0300

    ItemPedido e ItemPedidoPK parte 2

--- src/main/java/com/jharbes/cursomc/domain/Produto.java
+++ src/main/java/com/jharbes/cursomc/domain/Produto.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -21,25 +22,23 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 public class Produto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private Double preco;
-	
+
 	@JsonBackReference
 	@ManyToMany
-	@JoinTable(name="PRODUTO_CATEGORIA",
-		joinColumns=@JoinColumn(name="produto_id"),
-		inverseJoinColumns=@JoinColumn(name="categoria_id") 
-	)
-	private List<Categoria> categorias=new ArrayList<>();
-	
-	private Set<ItemPedido> itens=new HashSet<>();
-	
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
+
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+
 	public Produto() {
-		
+
 	}
 
 	public Produto(Integer id, String nome, Double preco) {
@@ -48,10 +47,10 @@ public class Produto implements Serializable {
 		this.nome = nome;
 		this.preco = preco;
 	}
-	
-	public List<Pedido> getPedidos(){
-		List<Pedido> lista=new ArrayList<>();
-		for (ItemPedido x: itens) {
+
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
 			lista.add(x.getPedido());
 		}
 		return lista;
@@ -88,7 +87,7 @@ public class Produto implements Serializable {
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
-	
+
 	public Set<ItemPedido> getItens() {
 		return itens;
 	}
@@ -113,5 +112,5 @@ public class Produto implements Serializable {
 		Produto other = (Produto) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
