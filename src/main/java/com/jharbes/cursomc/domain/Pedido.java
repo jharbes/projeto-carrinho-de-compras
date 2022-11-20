dad939bd1c8f7a74698a5256707836be61dd3b0c
Jorge Harbes <jharbes@hotmail.com>
2022-11-20 11:38:17 -0300

    ItemPedido e ItemPedidoPK parte 1

--- src/main/java/com/jharbes/cursomc/domain/Pedido.java
+++ src/main/java/com/jharbes/cursomc/domain/Pedido.java
@@ -2,7 +2,9 @@ package com.jharbes.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -33,6 +35,8 @@ public class Pedido implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
+	
+	private Set<ItemPedido> itens=new HashSet<>();
 
 	public Pedido() {
 		super();
@@ -84,6 +88,14 @@ public class Pedido implements Serializable {
 
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
--- src/main/java/com/jharbes/cursomc/domain/Produto.java
+++ src/main/java/com/jharbes/cursomc/domain/Produto.java
@@ -2,8 +2,10 @@ package com.jharbes.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -34,6 +36,8 @@ public class Produto implements Serializable {
 	)
 	private List<Categoria> categorias=new ArrayList<>();
 	
+	private Set<ItemPedido> itens=new HashSet<>();
+	
 	public Produto() {
 		
 	}
@@ -43,6 +47,14 @@ public class Produto implements Serializable {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista=new ArrayList<>();
+		for (ItemPedido x: itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
 	}
 
 	public Integer getId() {
@@ -76,6 +88,14 @@ public class Produto implements Serializable {
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public int hashCode() {
@@ -93,6 +113,5 @@ public class Produto implements Serializable {
 		Produto other = (Produto) obj;
 		return Objects.equals(id, other.id);
 	}
-	
 	
 }
